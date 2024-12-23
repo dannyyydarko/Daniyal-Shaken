31982cf76f7c6d0815ca451f9452ceb533bab86d
dannyyydarko <156187081+dannyyydarko@users.noreply.github.com>
2024-12-23 15:36:06 +0500

    Update Cylinder.java

--- Cylinder.java
+++ Cylinder.java
@@ -1,1 +1,23 @@
+public class Cylinder {
+    private double radius;
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    public double surfaceArea() {
+        return 2 * Math.PI * radius * (radius + height);
+    }
+
+    public double volume() {
+        return Math.PI * radius * radius * height;
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder with radius: " + radius + " and height: " + height;
+    }
+}
 
