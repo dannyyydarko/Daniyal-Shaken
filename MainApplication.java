4ddc76dbb55f7cf8e5010f9ce22552e102944bee
dannyyydarko <156187081+dannyyydarko@users.noreply.github.com>
2024-12-23 15:40:22 +0500

    Update MainApplication.java

--- MainApplication.java
+++ MainApplication.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter radius for the circle: ");
+        double radius = scanner.nextDouble();
+        System.out.print("Enter height for the cylinder: ");
+        double height = scanner.nextDouble();
+
+        Circle circle = new Circle(radius);
+        Cylinder cylinder = new Cylinder(radius, height);
+
+        System.out.println("Circle Area: " + circle.area());
+        System.out.println("Circle Circumference: " + circle.circumference());
+        System.out.println("Cylinder Surface Area: " + cylinder.surfaceArea());
+        System.out.println("Cylinder Volume: " + cylinder.volume());
+    }
+}
 
