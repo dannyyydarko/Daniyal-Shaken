013b7b249384e6db15a18e7de83bb3173a7b1ede
dannyyydarko <156187081+dannyyydarko@users.noreply.github.com>
2024-12-23 15:35:45 +0500

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,1 +1,20 @@
-Circle.java
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double circumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle with radius: " + radius;
+    }
+}
